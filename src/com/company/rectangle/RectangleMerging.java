ef406664bb1333a52f18384b3adfbd10adc1b8ee
olesyaivanovna00 <olesya.qwq@mail.ru>
2020-11-26 19:43:48 +0300

    почти готово

--- src/com/company/rectangle/RectangleMerging.java
+++ src/com/company/rectangle/RectangleMerging.java
@@ -2,11 +2,11 @@ package com.company.rectangle;
 
 import com.company.point.RealPoint;
 
-import java.awt.*;
+
 import java.util.*;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 
 public class RectangleMerging {
@@ -16,6 +16,8 @@ public class RectangleMerging {
     }
 
     private List<RealPoint> calcPoints(List<Rectangle> rectangles) {
+        boolean checker = false;
+
         List<RealPoint> ret = new ArrayList<>();
 
         List<Double> yCords = new ArrayList<>(getAllYCords(rectangles));
@@ -24,6 +26,7 @@ public class RectangleMerging {
 
         double previousLeftCoord = 0.0;
         double previousRightCoord = 0.0;
+        List<Double> previousXCords = new ArrayList<>();
 
         for (double xCord : xCords
         ) {
@@ -45,21 +48,81 @@ public class RectangleMerging {
                 if (!minimumXLeftCord.equals(previousLeftCoord)) {
                     ret.add(0, new RealPoint(previousLeftCoord, yCord));
                 }
+
+                if (rectsOverYBottomLine(yCord, rectangles).size() > 1){
+
+                    checker = true;
+                }
+
                 ret.add(0, new RealPoint(minimumXLeftCord, yCord));
 
                 if (!maximumXRightCord.equals(previousRightCoord)) {
                     ret.add(new RealPoint(previousRightCoord, yCord));
                 }
+                
+
 
             }
-            ret.add(new RealPoint(maximumXRightCord, yCord));
 
+
+            ret.add(new RealPoint(maximumXRightCord, yCord));
             previousLeftCoord = minimumXLeftCord;
             previousRightCoord = maximumXRightCord;
         }
 
+        if(checker){
+            ret = addPoints(rectangles, yCords, ret);
+        }
+
         return ret;
 
+    }
+
+    private List<RealPoint> addPoints(List<Rectangle> rets, List<Double> yCords, List<RealPoint> polygon){
+
+
+        for (double yCord : yCords) {
+            Double minimumXLeftCord = minXLeftCoord(yCord, rets);
+            Double maximumXRightCord = maxXRightCoord(yCord, rets);
+
+            if(rectsOverYBottomLine(yCord, rets).size() > 1){
+                List<Rectangle> rectsOverYBottomLine = rectsOverYBottomLine(yCord, rets);
+                List<Double> xCords = new ArrayList<>(getAllXCords(rectsOverYBottomLine));
+                xCords.sort(Comparator.naturalOrder());
+
+                //int index = indexOfCord(polygon, new RealPoint(minimumXLeftCord,yCord));
+//                int index = polygon.indexOf(new RealPoint(xCords.get(0),yCord));
+                //System.out.println(index);
+                for (double xCord:xCords) {
+                    int index = indexOfCord(polygon, new RealPoint(minimumXLeftCord,yCord));
+                    System.out.println(index);
+
+                    polygon.add(index, new RealPoint(xCord, yCord));
+                }
+
+            }
+
+
+        }
+
+
+        return polygon;
+    }
+
+    private int prevIndexOfCord(List<RealPoint> polygon, RealPoint realPoint){
+        for (RealPoint rp:polygon) {
+
+        }
+        return -1;
+    }
+
+
+    private int indexOfCord(List<RealPoint> polygon, RealPoint realPoint){
+        for (RealPoint rp:polygon) {
+            if((rp.getX() == realPoint.getX())&& (rp.getY() == realPoint.getY()))
+                return polygon.indexOf(rp);
+        }
+        return -1;
     }
 
     private Set<Double> getAllXCords(List<Rectangle> rectangles) {
@@ -110,16 +173,14 @@ public class RectangleMerging {
 
     }
 
+
     private List<Rectangle> rectanglesAtY(double y, List<Rectangle> rectangles) {
         List<Rectangle> rectsAtYBottomLines = rectsAtYBottomLines(y, rectangles);
-        if (rectsAtYBottomLines.size() == 1){
+        if (rectsAtYBottomLines.size() == 1) {
             return rectsAtYBottomLines;
         } else {
 
-//            for (Rectangle r: rectsAtYBottomLines) {
-//                r.
-//            }
-            return rectsAtYBottomLines;
+            return rectsOverYBottomLine(y, rectangles);
         }
 
         //надо понять пересекается ли он с чем-то
@@ -127,6 +188,15 @@ public class RectangleMerging {
 
     }
 
+    private List<Rectangle> rectsOverYBottomLine(double y, List<Rectangle> rectangles){
+        List<Rectangle> rectsAtYBottomLines = new ArrayList<>();
+        for (Rectangle r : rectangles) {
+            if (r.getTop().getY() > y && r.getBottom().getY() < y) {
+                rectsAtYBottomLines.add(r);
+            }
+        }
+        return rectsAtYBottomLines;
+    }
 
     private List<Rectangle> rectsAtYBottomLines(double y, List<Rectangle> rectangles) {
         List<Rectangle> rectsAtYBottomLines = new ArrayList<>();
@@ -143,6 +213,35 @@ public class RectangleMerging {
 
     }
 
+    private List<Rectangle> rectsInXLine(double x, List<Rectangle> rectangles){
+        List<Rectangle> rectsInXLine = new ArrayList<>();
+        for (Rectangle r:rectangles) {
+            if (r.getTop().getX() == x || r.getRight().getX() == x){
+                rectsInXLine.add(r);
+            }
+
+            if (r.getTop().getX() < x && r.getRight().getX() > x){
+                rectsInXLine.add(r);
+            }
+        }
+
+        return rectsInXLine;
+    }
+
+    private List<Rectangle> rectsCrossedInYLine(List<Rectangle> rectanglesInY){
+        List<Rectangle> rectsCrossedInYLine = new ArrayList<>();
+        List<Double> xCords = new ArrayList<>(getAllXCords(rectanglesInY));
+        xCords.sort(Comparator.naturalOrder());
+
+        for (Double x:xCords) {
+            if (rectsInXLine(x, rectanglesInY).size() <= 2){
+                rectsCrossedInYLine.addAll(rectsInXLine(x, rectanglesInY));
+            }
+        }
+
+
+        return rectsCrossedInYLine;
+    }
 
 
 }
